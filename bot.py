ebc6ca9c6f4ffab769d1cfe85e6544a32ded79a2
elNoisY <159972832+elNoisY@users.noreply.github.com>
2026-08-04 12:57:56 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,7 +130,7 @@ if cookies_b64:
         print(f"⚠️ Error al procesar las cookies: {e}")
 
 YTDL_OPTIONS = {
-    'format': 'bestaudio/best/bestvideo+bestaudio',
+    'format': 'best',
     'outtmpl': '/tmp/%(id)s.%(ext)s',
     'noplaylist': True,
     'nocheckcertificate': True,
